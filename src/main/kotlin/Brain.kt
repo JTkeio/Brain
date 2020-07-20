954580c152f5b6c7dd7b47f1e918d3cdc3fe5f5f
JTkeio <jacobtkeio@gmail.com>
2020-07-19 23:37:08 -0700

    Transitioned from Int type to Array(ranges.size){-1} for default neurons to avoid type shenanigans. Also changed all 0 until for loops to .indices calls where applicable

--- src/main/kotlin/Brain.kt
+++ src/main/kotlin/Brain.kt
@@ -4,12 +4,12 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
     val dimensions = dimensions //each entry is a new input, the value represents their possible values 0-n
     val ranges = ranges //each entry is a new output, the value represents their possible values 0-n
     val numberOfNeurons = multiplyArray(dimensions)
-    val brain = Array<Any>(numberOfNeurons){0}
+    val brain = Array(numberOfNeurons){Array(ranges.size){-1}} //initialize brain with arrays fitting the constraints from ranges but filled with -1 to signify emptiness
 
     fun multiplyArray(array: Array<Int>): Int {
     var initial = 1
-    for (v in 0 until array.size) {
-        initial *= array[v]
+    for (v in array) {
+        initial *= v
     }
     return initial
 } //multiplies every integer in an array, converts an any-dimensional size into its one-dimensional size
@@ -22,7 +22,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
         var linearAddress = 0
         var tempNumberOfNeurons= multiplyArray(tempDimensions)
 
-        for (x in 0 until tempDimensions.size) {
+        for (x in tempDimensions.indices) {
             tempNumberOfNeurons /= tempDimensions[x]
             linearAddress += tempNumberOfNeurons*dimensionalAddress[x]
         }
@@ -40,7 +40,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
         val dimensionalAddress = Array(tempDimensions.size){0}
         var tempLinearAddress = linearAddress
 
-        for (z in 0 until tempDimensions.size) {
+        for (z in tempDimensions.indices) {
             tempNumberOfNeurons /= tempDimensions[z]
             while (tempLinearAddress >= tempNumberOfNeurons) {
                 dimensionalAddress[z] += 1
@@ -52,9 +52,13 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
 
     fun pushNeuron(dimensionalAddress:Array<Int>, values: Array<Int>): Int {
         val tempAddress = getLinear(dimensionalAddress, dimensions)
+        if (tempAddress<0 || tempAddress>numberOfNeurons) {
+            println("invalid dimensional address when pushing a neuron!")
+            return -3
+        }
 
         if (values.size == ranges.size) {
-            for (g in 0..values.size - 1) {
+            for (g in values.indices) {
                 if ((values[g] > ranges[g]) || (values[g] < 0)) {
                     println("Extraneous proposed values!")
                     return -1
@@ -72,40 +76,45 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
             print("Invalid dimensional address")
             return emptyArray()
         }
-        val neuron = brain[getLinear(dimensionalAddress, dimensions)]
+        val neuronIndex = getLinear(dimensionalAddress, dimensions)
+        if (neuronIndex<0 || neuronIndex>numberOfNeurons) {
+            println("invalid dimensional address when pulling a neuron!")
+            return emptyArray()
+        }
+        val neuron = brain[neuronIndex]
 
-        if (neuron is Int) {
+        if (neuron[0]<0) {
             val newNeuron = generateNeuronProximityAverage(dimensionalAddress, searchGranularity) //The neuron does not exist, construct it
             brain[getLinear(dimensionalAddress, dimensions)] = newNeuron //put the new neuron back into the brain
             return newNeuron //send it through!
         } else {
-            return neuron as Array<Int> //The neuron exists, send it through!
+            return neuron //The neuron exists, send it through!
         }
 
     } //C1: no neuron exists, create one from surrounding neurons \/ C2: neuron exists, return data
 
     fun generateNeuronProximityAverage(dimensionalAddress: Array<Int>, searchGranularity: Int): Array<Int> {
         if (searchGranularity < 1) { return generateNeuronRandom() }
-        val neuronTypes = Array<Int>(2){0}
-        val amalgamNeuron = Array<Int>(ranges.size){0}
-        val bubbleArray = Array<Int>(dimensions.size){(2*searchGranularity)+1} //bubble refers to the shape :) this defines the size and dimensions of the search bubble
+        val neuronTypes = Array(2){0}
+        val amalgamNeuron = Array(ranges.size){0}
+        val bubbleArray = Array(dimensions.size){(2*searchGranularity)+1} //bubble refers to the shape :) this defines the size and dimensions of the search bubble
 
         for (k in 0 until multiplyArray(bubbleArray)) {
             val searchArray = getDimensional(k, bubbleArray) //searchArray is constructed with coordinates for each point as defined by bubbleArray
-            for (r in 0 until searchArray.size) {
+            for (r in searchArray.indices) {
                 searchArray[r]-=searchGranularity //the coordinates need to be shifted so they are centered on the seed
             }
-            val searchAddress = getLinear(dimensionalAddress, dimensions) + getLinear(searchArray, dimensions)
+            val searchAddress = getLinear(dimensionalAddress, dimensions) + getLinear(searchArray, dimensions) //overlay the seed with searchArray to move the search neuron
             if (searchAddress<0 || searchAddress>numberOfNeurons-1) {
                 continue //check for extraneous searches :P fencepost issue on numberOfNeurons
             }
-            val searchNeuron = brain[searchAddress] //overlay the seed with searchArray to move the search neuron
-            if (searchNeuron is Int) {
+            val searchNeuron = brain[searchAddress]
+            if (searchNeuron[0]<0) {
                 neuronTypes[0]+=1 //this neuron is empty
             } else {
                 neuronTypes[1]+=1 //this neuron is full
-                for (l in 0 until (searchNeuron as Array<*>).size) {
-                    amalgamNeuron[l] += searchNeuron[l] as Int //add the full neuron to amalgam so it can be averaged later
+                for (l in searchNeuron.indices) {
+                    amalgamNeuron[l] += searchNeuron[l] //add the full neuron to amalgam so it can be averaged later
                 }
             }
         }
@@ -113,7 +122,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
 
         if (Random.nextDouble(0.0, neuronTypes.sum().toDouble()) < neuronTypes[0]/neuronTypes.sum()) { //a bit of randomness based on the % good/bad neurons gathered
             for (p in 0 until amalgamNeuron.size) {
-                amalgamNeuron[p] /= if (neuronTypes[1] != 0) (neuronTypes[1]) else (1) //amalgam neuron is just a summation so far, but it should be an average
+                amalgamNeuron[p] /= if (neuronTypes[1] != 0) (neuronTypes[1]) else (1) //amalgam neuron is just a summation so far, but it should be an average. DO NOT divide by 0
             }
             return amalgamNeuron //there were enough good neurons around this one, so it will become an average of them
         } else {
@@ -123,7 +132,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
 
     fun generateNeuronRandom(): Array<Int> {
         val newNeuron = Array(ranges.size){0}
-        for (h in 0 until ranges.size) {
+        for (h in ranges.indices) {
             newNeuron[h] = Random.nextInt(0,ranges[h]+1) //fencepost issue
         }
         return newNeuron
@@ -132,9 +141,9 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
     fun print(){
         for (u in 0 until numberOfNeurons) {
             val tempNeuron = brain[u]
-            if (tempNeuron is Int) {print("$tempNeuron ")} else if (tempNeuron is Array<*>){
+            if (tempNeuron[0]<0) {print("  ")} else {
                 for (w in tempNeuron) {
-                    print(w.toString()+",")
+                    print("$w,")
                 }
                 print(" ")
             }
@@ -142,7 +151,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
 
             val tempArray = getDimensional(u, dimensions)
             var tempSpacer: Boolean
-            for (t in 0 until tempArray.size) {
+            for (t in tempArray.indices) {
                 tempSpacer = true
                 for (r in t until tempArray.size) {
                     tempSpacer = tempSpacer and (tempArray[r]+1==dimensions[r])
@@ -152,18 +161,18 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
         }
     } //"displays" the brain.
 
-    fun printBinary(){
+    fun printBinaryImage(){
         for (u in 0 until numberOfNeurons) {
             val tempNeuron = brain[u]
-            if (tempNeuron is Int) {print("$tempNeuron ")} else if (tempNeuron is Array<*>){
-                if (tempNeuron.size > 1) {println("no size greater than one allowed!!!")} else if (tempNeuron[0] as Int > 0) {print("#")} else {print(" ")}
+            if (tempNeuron[0]<0) {print("  ")} else {
+                if (tempNeuron.size > 1) {println("no size greater than one allowed!!!")} else if (tempNeuron[0] > 0) {print("#")} else {print(" ")}
                 print(" ")
             }
 
 
             val tempArray = getDimensional(u, dimensions)
             var tempSpacer: Boolean
-            for (t in 0 until tempArray.size) {
+            for (t in tempArray.indices) {
                 tempSpacer = true
                 for (r in t until tempArray.size) {
                     tempSpacer = tempSpacer and (tempArray[r]+1==dimensions[r])
@@ -175,9 +184,9 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
 }
 
 fun main() {
-    val lmao = Brain(arrayOf(10,60), arrayOf(1))
+    val lmao = Brain(arrayOf(9,20), arrayOf(1))
     for (hj in 0 until lmao.numberOfNeurons) {
-        lmao.pullNeuron(lmao.getDimensional(hj, lmao.dimensions), 0)
+        lmao.pullNeuron(lmao.getDimensional(hj, lmao.dimensions), 3)
     }
-    lmao.printBinary()
+    lmao.printBinaryImage()
 }
