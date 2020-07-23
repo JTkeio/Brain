05f1dc3275bea684316bb8022548f2d6c1bcc85c
JTkeio <jacobtkeio@gmail.com>
2020-07-22 17:19:22 -0700

    Convert Geometry_Test to Brain_Tests and make the new geometryTest() function return it's accuracy

--- src/main/kotlin/Brain.kt
+++ src/main/kotlin/Brain.kt
@@ -1,9 +1,10 @@
 package jtkeio.brain
 import kotlin.random.Random
 
-class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
-    val dimensions = dimensions //each entry is a new input, the value represents their possible values 0-n
-    val ranges = ranges //each entry is a new output, the value represents their possible values 0-n
+//each entry in dimensions is a new input channel, the value represents each channel's possible values 0-n
+//each entry in ranges is a new output channel, the value represents each channel's possible values 0-n
+
+class Brain(val dimensions: Array<Int>, val ranges: Array<Int>) {
     val numberOfNeurons = multiplyArray(dimensions)
     val brain = Array(numberOfNeurons){Array(ranges.size){-1}} //initialize brain with arrays fitting the constraints from ranges but filled with -1 to signify emptiness
 
@@ -123,8 +124,8 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
     fun printBinaryImage(){
         for (u in 0 until numberOfNeurons) {
             val tempNeuron = brain[u]
-            if (tempNeuron[0]<0) {print("  ")} else {
-                if (tempNeuron.size > 1) {println("no size greater than one allowed!!!")} else if (tempNeuron[0] > 0) {print("#")} else {print(" ")}
+            if (tempNeuron[0]<0) {print("   ")} else {
+                if (tempNeuron.size > 1) {println("no size greater than one allowed!!!")} else if (tempNeuron[0] > 0) {print("#")} else {print(". ")}
                 print(" ")
             }
 
@@ -178,7 +179,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
         neuronTypes[0]-=1 //remove the seed (dimensionalAddress), which is always an Int, from the tally
 
         if (Random.nextDouble(0.0, neuronTypes.sum().toDouble()) < neuronTypes[0]/neuronTypes.sum()) { //a bit of randomness based on the % good/bad neurons gathered
-            for (p in 0 until amalgamNeuron.size) {
+            for (p in amalgamNeuron.indices) {
                 amalgamNeuron[p] /= if (neuronTypes[1] != 0) (neuronTypes[1]) else (1) //amalgam neuron is just a summation so far, but it should be an average. DO NOT divide by 0
             }
             return amalgamNeuron //there were enough good neurons around this one, so it will become an average of them
@@ -188,7 +189,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
     } //my algorithm for generating neurons based on previous information
 
     fun generateNeuronProximityAverageAbsolute(dimensionalAddress: Array<Int>, searchGranularity: Int): Array<Int> {
-        if (searchGranularity < 1) { return generateNeuronRandom() }
+        if (searchGranularity < 0) { return generateNeuronRandom() }
         val neuronTypes = Array(2){0}
         val amalgamNeuron = Array(ranges.size){0}
         val bubbleArray = Array(dimensions.size){(2*searchGranularity)+1} //bubble refers to the shape :) this defines the size and dimensions of the search bubble
@@ -214,7 +215,7 @@ class Brain(dimensions:Array<Int>, ranges:Array<Int>) {
         }
         neuronTypes[0]-=1 //remove the seed (dimensionalAddress), which is always an Int, from the tally
 
-        if (neuronTypes[0]<neuronTypes[1]) { //removed randomness
+        if (neuronTypes[0]<=neuronTypes[1]) { //removed probability
             for (p in amalgamNeuron.indices) {
                 amalgamNeuron[p] /= if (neuronTypes[1] != 0) (neuronTypes[1]) else (1) //amalgam neuron is just a summation so far, but it should be an average. DO NOT divide by 0
             }
---
+++ src/test/kotlin/Brain Tests.kt
@@ -0,0 +1,65 @@
+package jtkeio.brain
+import kotlin.random.Random
+
+
+fun main() {
+    for (lmao in 0..100) {
+        println(lmao.toDouble()/100)
+        println(geometryTest(arrayOf(100, 100), 2, lmao.toDouble()/100, false))
+    }
+}
+
+
+fun geometryTest(dimensions: Array<Int>, searchGranularity: Int, percentageInformation: Double, doPrint: Boolean): Double {
+
+    // NOTE from brainboy:
+        //This test demonstrates the effectiveness of Brain's algorithms. I recommend generateNeuronProximityAverageAbsolute. It seems to work best here.
+        //That being said, generateNeuronProximityAverageProbability could be better when some amount of variance is expected of a neural net
+        //To demonstrate, a 2D graph is filled partially with correct information at random points, then geometryBrain guesses what should go at other random points based on
+            //what it sees around it, and then goes in order through all remaining points just so that the graph isn't half empty
+        //The algorithm must be changed in the Brain class file, but I'm working on a way to set it like a variable <3
+        //The code running Brain can also give it feedback by pushing correct neurons or erasing incorrect neurons during runtime. For accuracy's sake, I didn't bother with that.
+        //Finally, Brain is capable of multiple outputs of any range (besides negative) but it doesn't make sense to do more than two here. That demo will come soon!
+    //Have fun, and beware of text stretch factors :)
+
+    val geometryBrain = Brain(dimensions, arrayOf(1)) //here you can define what your dimensions are in y to x order to fit your screen, but don't change the arrayOf(1) :p
+    val searchGranularity = searchGranularity //how wide one neuron will search for info. Bigger isn't always better! 0 is random
+    val percentageInformation = percentageInformation //how much information you give geometryBrain to start with as a decimal percentage. It's not entirely accurate
+
+    fun geometricEquation(searchAddress: Array<Int>): Boolean {
+        //define the equation you want geometryBrain to replicate here, or choose one of these
+
+        //return (searchAddress[0]-15).toFloat().pow(2) + (searchAddress[1]-15).toFloat().pow(2) > 30 //a circle
+        //return searchAddress[0]>25 //horizontal line
+        //return searchAddress[1]>25 //vertical line
+        return searchAddress[0] + searchAddress[1] > 100 //slanted line
+    }
+
+    for (i in 0 until (geometryBrain.numberOfNeurons*percentageInformation).toInt()) {
+        val tempAddress = geometryBrain.getDimensional(Random.nextInt(0, geometryBrain.numberOfNeurons), geometryBrain.dimensions)
+        geometryBrain.pushNeuron(tempAddress, if (geometricEquation(tempAddress)) (arrayOf(1)) else (arrayOf(0)))
+    } //puts in the correct information at random points. The amount of random (but correct!) information is determined by percentageInformation
+
+    if (doPrint) {geometryBrain.printBinaryImage()} //print the information that was inserted randomly
+
+    for (j in 0 until geometryBrain.numberOfNeurons*2) {
+        val tempAddress = geometryBrain.getDimensional(Random.nextInt(0, geometryBrain.numberOfNeurons), geometryBrain.dimensions)
+        geometryBrain.pullNeuron(tempAddress, searchGranularity)
+    } //arbitrarily looks at random points and guesses what should go there. For now, the algorithm being used has to be change in the Brain class file.
+
+    for (k in 0 until geometryBrain.numberOfNeurons) {
+        val tempAddress = geometryBrain.getDimensional(k, geometryBrain.dimensions)
+        geometryBrain.pullNeuron(tempAddress, searchGranularity)
+    } //go through all remaining neurons and guess (pullNeuron() does nothing if the neuron in question is already full so there is no negative to running back through all neurons)
+
+    if (doPrint) {geometryBrain.printBinaryImage()} //print all of the brain, including what we just constructed
+
+    var accuracy = 0
+    for (l in 0 until geometryBrain.numberOfNeurons) {
+        val searchAddress = geometryBrain.getDimensional(l, geometryBrain.dimensions)
+        if ((geometryBrain.pullNeuron(searchAddress,0)[0] == if (geometricEquation(searchAddress)) (1) else (0))) {
+            accuracy += 1
+        }
+    }
+    return accuracy/geometryBrain.numberOfNeurons.toDouble() //return the accuracy of geometryBrain as a decimal percentage
+}
