0ade8912051d209e29099c49e54a0c838bbf78ed
JTkeio <jacobtkeio@gmail.com>
2020-12-23 14:56:27 -0800

    add grabColorBMP() to ImageBrain.kt

--- src/test/kotlin/ImageBrain.kt
+++ src/test/kotlin/ImageBrain.kt
@@ -1,2 +1,26 @@
 package jtkeio.brain
+import java.io.File
 
+
+
+fun grabColorBMP(path: String, color: String): Array<Int> {
+    val a = File(path).readBytes()
+    val numberOfPixels = (a.size-52)/3
+    val colorPixels = Array(numberOfPixels){-1}
+
+    val colorShift = when(color){
+        "r", "R" -> 0
+        "g", "G" -> 2
+        "b", "B" -> 1
+        else -> -0
+    } //default to red
+
+    for (i in 0 until numberOfPixels) {
+        colorPixels[i] = a[3*i+53+colorShift].toInt()
+    }
+    return colorPixels
+} //returns a list of values per color per pixels of a BMP
+
+fun main() {
+    grabColorBMP("C:/Users/Jacob Tkeio/Desktop/Programs/Kotlin Projects/Batman.bmp", "r")
+}
